480fc5cf1a2de238ee48782fea3ab791c2269e9d
Saikat Sinha Ray <saikat.sinharay@gmail.com>
2023-10-29 02:17:05 +0530

    Update encrypted-message.py

--- python-problems/encrypted-message.py
+++ python-problems/encrypted-message.py
@@ -2,34 +2,20 @@
 Description
 In a certain encrypted message which has information about the location(area, city), the characters are jumbled such that the first character of the first word is followed by the first character of the second word, then it is followed by the second character of the first word and so on.
 
-
-
 In other words, let’s say the location is bandra,mumbai
-
-
 
 The encrypted message says ‘bmaunmdbraai’.
 
-
-
-Sample Input:
-
+#Sample Input:
 bmaunmdbraai
 
-
-
-Sample Output:
-
+#Sample Output:
 bandra,mumbai
-
-
 
 Let’s say the size or length of the two words wouldn’t match, then the smaller word is appended with # and then encrypted in the above format.
 
-
-
 With this in mind, write a code to identify the right location and print it as place, city.
 
-Execution Time Limit
+#Execution Time Limit
 10 seconds
 """
